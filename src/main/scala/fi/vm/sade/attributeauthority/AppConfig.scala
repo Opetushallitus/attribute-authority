34f098c72e0c8d9bed31bc7d59f19c2ec6e3a4bd
Riku Salminen <riku.salminen@cybercom.com>
2014-09-18 15:06:37 +0300

    Propertyjen käsittely.

--- src/main/scala/fi/vm/sade/attributeauthority/AppConfig.scala
+++ src/main/scala/fi/vm/sade/attributeauthority/AppConfig.scala
@@ -24,34 +24,44 @@ object AppConfig {
     val conf = ConfigFactory.load(ConfigFactory.parseFile(file))
 
     private def stringFromProps(key: String, default: String): String = {
-      conf.getString(key) match {
-        case s: String if !s.isEmpty => s
-        case _ => default
+      try {
+        conf.getString(key) match {
+          case null => default
+          case s => s
+        }
+      }
+      catch {
+        case _: ConfigException => default
       }
     }
 
     private def intFromProps(key: String, default: Int): Int = {
-      conf.getInt(key) match {
-        case i: Int if i != 0 => i
-        case _ => default
+      try {
+        conf.getInt(key) match {
+          case i: Int if i != 0 => i
+          case _ => default
+        }
+      }
+      catch {
+        case _: ConfigException => default
       }
     }
 
-    override val saml2IssuerUrl: String = stringFromProps("saml2_issuer_url", saml2IssuerUrl)
-    override val saml2NameID: String = stringFromProps("saml2_name_id", saml2NameID)
-    override val saml2ResponseValidityTimeSeconds: Int = intFromProps("saml2_response_validity_time_seconds", saml2ResponseValidityTimeSeconds)
+    override def saml2IssuerUrl: String = stringFromProps("attributeauthority.saml2_issuer_url", super.saml2IssuerUrl)
+    override def saml2NameID: String = stringFromProps("attributeauthority.saml2_name_id", super.saml2NameID)
+    override def saml2ResponseValidityTimeSeconds: Int = intFromProps("attributeauthority.saml2_response_validity_time_seconds", super.saml2ResponseValidityTimeSeconds)
     
-    override val henkilohallintaUrl: String = stringFromProps("henkilohallinta_url", henkilohallintaUrl)
-    override val henkilohallintaUsername: String = stringFromProps("henkilohallinta_username", henkilohallintaUsername)
-    override val henkilohallintaPassword: String = stringFromProps("henkilohallinta_password", henkilohallintaPassword)
+    override def henkilohallintaUrl: String = stringFromProps("attributeauthority.henkilohallinta_url", super.henkilohallintaUrl)
+    override def henkilohallintaUsername: String = stringFromProps("attributeauthority.henkilohallinta_username", super.henkilohallintaUsername)
+    override def henkilohallintaPassword: String = stringFromProps("attributeauthority.henkilohallinta_password", super.henkilohallintaPassword)
 
-    override val casTicketUrl: String = stringFromProps("cas_ticket_url", casTicketUrl)
-    override val ticketConsumerUrl: String = stringFromProps("cas_ticket_consumer_url", casTicketUrl)
+    override def casTicketUrl: String = stringFromProps("attributeauthority.cas_ticket_url", super.casTicketUrl)
+    override def ticketConsumerUrl: String = stringFromProps("attributeauthority.cas_ticket_consumer_url", super.ticketConsumerUrl)
 
-    override val authenticationInfoService: AuthenticationInfoService = casTicketUrl match {
+    override def authenticationInfoService: AuthenticationInfoService = casTicketUrl match {
       case url if !url.isEmpty => new RemoteAuthenticationInfoService(RemoteApplicationConfig(
         url, henkilohallintaUsername, henkilohallintaPassword, henkilohallintaUrl, ticketConsumerUrl))
-      case _ => authenticationInfoService
+      case _ => super.authenticationInfoService
     }
 
 
@@ -59,15 +69,15 @@ object AppConfig {
 }
 
 trait AppConfig {
-  val saml2IssuerUrl: String = "https://localhost/oidresolver"
-  val saml2NameID: String = "TestID"
-  val saml2ResponseValidityTimeSeconds: Int = 3600
-  val henkilohallintaUrl: String = ""
-  val henkilohallintaUsername: String = ""
-  val henkilohallintaPassword: String = ""
-  val casTicketUrl: String = ""
-  val ticketConsumerUrl: String = ""
-  val authenticationInfoService: AuthenticationInfoService = new MockAuthenticationInfoService
+  def saml2IssuerUrl: String = "https://localhost/oidresolver"
+  def saml2NameID: String = "TestID"
+  def saml2ResponseValidityTimeSeconds: Int = 3600
+  def henkilohallintaUrl: String = ""
+  def henkilohallintaUsername: String = ""
+  def henkilohallintaPassword: String = ""
+  def casTicketUrl: String = ""
+  def ticketConsumerUrl: String = ""
+  def authenticationInfoService: AuthenticationInfoService = new MockAuthenticationInfoService
 }
 
 case class RemoteApplicationConfig(casUrl: String, username: String, password: String, henkilohallintaUrl: String, ticketConsumerUrl: String)
